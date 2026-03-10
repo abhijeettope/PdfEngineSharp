c2393c126ef6e80fded71ad2bf79893559652ec8
abhijeettope <abhijeettope@gmail.com>
2026-03-10 23:45:03 +0530

    added console app for sample file creation

---
+++ src/Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using PdfEngineSharp;
+using PdfEngineSharp.Shapes;
+
+Console.WriteLine("Hello from sample for PDF Engine Sharp...");
+
+Pdf myPdf = new Pdf();
+
+myPdf.Add(1, new PdfLine(
+    new PdfPoint(10, 10),
+    new PdfPoint(100, 100)
+    ));
+
+string output = myPdf.Get();
+
+Console.WriteLine("Output string of PDF.");
+Console.WriteLine(output);
